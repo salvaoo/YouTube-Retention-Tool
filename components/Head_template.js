e103a284220bd092ed239f7f151ce5dbeec98bf1
salvaoo <salvadorgilrosales@outlook.es>
2022-03-20 22:15:01 +0000

    Added GA | 90%

--- components/Head_template.js
+++ components/Head_template.js
@@ -1,12 +1,39 @@
 import Head from 'next/head'
-
+import { useEffect } from 'react'
 
 function Head_template({ title, description_content, icon }) {
+
+  useEffect(() => {
+    let host = window.location.hostname;
+    if (host === "localhost") {
+      window[`ga-disable-${process.env.GOOGLE_ANALYTICS}`] = true;
+      console.log('GA disabled');
+    }else {
+      console.log('GA enable');
+    }
+  }, [])
+
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description_content} />
+      <meta name="author" content="salvagr.com" />
       <link rel="icon" href={icon} />
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+        dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+            page_path: window.location.pathname,
+          });`,
+          }}
+        />
     </Head>
   );
 }
