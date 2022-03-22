1d120855fc609f5a9c14f49144133a0210d17beb
salvaoo <salvadorgilrosales@outlook.es>
2022-03-22 02:06:23 +0000

    Fixed noreferrer error | 93%

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { FcLike } from 'react-icons/fc'
 function Footer() {
   return (
     <footer className="relative bg-black text-white py-5 flex gap-3 justify-center items-center">
-      <p className="text-sm">Site created by <a className='text-red-200' href="https://salvagr.com" target="_blank">salvagr.com</a></p>
+      <p className="text-sm">Site created by <a className='text-red-200' href="https://salvagr.com" target="_blank" rel="noreferrer">salvagr.com</a></p>
       <FcLike />
     </footer>
   )
